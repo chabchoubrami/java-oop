d15d4a6c7ccf92a3a95f648a26172288243c1385
rami chabchoub <ramichabchoub.rc@gmail.com>
2023-08-05 13:07:00 +0100

    static keyword, PART 2

--- src/main/java/com/mycompany/OuterClass.java
+++ src/main/java/com/mycompany/OuterClass.java
@@ -1,2 +1,12 @@
-package com.mycompany;public class OuterClass {
+package com.mycompany;
+
+public class OuterClass {
+    static int x = 10;
+
+    public static class InnerClass {
+        public void print() {
+            System.out.println("This is an inner class");
+            System.out.println("x = " + x);
+        }
+    }
 }
