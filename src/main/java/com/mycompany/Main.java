5704de608cfd12185cd3b42112f360c868f7f39b
rami chabchoub <ramichabchoub.rc@gmail.com>
2023-08-06 18:54:39 +0100

    Passing Objects to Methods (value, reference)

--- src/main/java/com/mycompany/Main.java
+++ src/main/java/com/mycompany/Main.java
@@ -1,25 +1,14 @@
 package com.mycompany;
 
 public class Main {
-    static int x = 10;
-    int y = 11;
+    static void fun(Test obj) {
+        obj.number++;
+    }
 
-    static {
-        x = 20;
-        System.out.println("This is a static block");
-    }
-    static {
-        x = 30;
-        System.out.println("This is a static block 2");
-    }
     public static void main(String[] args) {
-        Main m = new Main();
-        System.out.println(x);
-        System.out.println(m.y);
-
-        Student student = new Student("Sara", "123");
-
-        OuterClass.InnerClass ic = new OuterClass.InnerClass();
-        ic.print();
+        Test obj = new Test();
+        obj.number = 1;
+        fun(obj);
+        System.out.println("number = " + obj.number);
     }
 }
